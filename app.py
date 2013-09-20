b890be263c3008e419107aed22d4a633d20be33e
Mike Zamansky <zamansky@gmail.com>
2013-09-20 13:00:31 -0400

    Added some routes

--- app.py
+++ app.py
@@ -3,7 +3,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "<h1>This is the home page </h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>This is the about page</h1>"
+    
+
 
 if __name__=="__main__":
-    app.run()
+    app.run(host="0.0.0.0",port=5005)
     
