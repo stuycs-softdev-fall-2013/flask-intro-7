1539671f9c1280d603b29640e78a90f5cd0c8100
Mike Zamansky <zamansky@gmail.com>
2013-09-20 13:09:41 -0400

    Added who route

--- app.py
+++ app.py
@@ -7,6 +7,18 @@ app = Flask(__name__)
 def home():
     return "<h1>This is the home page </h1>"
 
+@app.route("/who")
+@app.route("/who/<name>")
+def name(name="default"):
+    page = """
+    <h1> the page name </h1>
+    This is a page with someone's name
+    <hr>
+    The name is: 
+    """
+    page=page+name+"<hr>"
+    return page
+
 @app.route("/about")
 def about():
     return "<h1>This is the about page</h1>"
@@ -14,5 +26,6 @@ def about():
 
 
 if __name__=="__main__":
+    app.debug=True
     app.run(host="0.0.0.0",port=5005)
     
